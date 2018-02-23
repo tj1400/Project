92eef7b7e76527aa2fc513d3f2917f61401667c8
andrewlbainbridge <35848427+andrewlbainbridge@users.noreply.github.com>
2018-02-23 15:32:21 -0800

    Add files via upload

---
+++ andrewB.cpp
@@ -0,0 +1,42 @@
+//Andrew Bainbridge
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include "fonts.h"
+
+
+
+
+bool OnGround = true;
+
+	void JumpUp()
+{}
+/*
+{
+    if (OnGround) {
+    render->cy += 11.0f;
+    OnGround = false;
+    }
+    else {
+    return; //char in air
+    }
+}
+	void UpdatePos()
+{
+    velY += gravity;
+    posY += velY;
+    posX += velX;
+    if (posY > ) {
+    posY =  ;
+    velY = 0.0;
+    OnGround = true;
+    }
+
+}
+*/
