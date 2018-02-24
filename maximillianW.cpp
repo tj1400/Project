da6a42a4d8b530b1ee308394df29bb5806dfdcd8
mwolfe242 <32782129+mwolfe242@users.noreply.github.com>
2018-02-24 15:33:46 -0800

    Add files via upload

--- maximillianW.cpp
+++ maximillianW.cpp
@@ -5,14 +5,16 @@ void showhealth(int health){
 	r.left = 400;
 	r.center = 400;
 	unsigned int c;
+	//color parameters----------------------
 	if(health>=80)
-	c=0x0000ff00;
+		c=0x0000ff00;
 	if(health>=50&&health<80)
-	c=0x00ffff00;
+		c=0x00ffff00;
 	if(health>=20&&health<50)
-	c=0x00ffa500;
+		c=0x00ffa500;
 	if(health<20)
-	c=0x00ff0000;
+		c=0x00ff0000;
+	//--------------------------------------
 	ggprint8b(&r, 16, c, "Health: %i",health);
 }
 
